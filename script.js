67a9c460d4e132ee8418275b6a7ee2eb460dc446
@CodeWithZaii <xairelnakar01@gmail.com>
2024-11-28 12:47:42 +0800

    .

---
+++ script.js
@@ -0,0 +1,38 @@
+//using charAt(); Takes index and it returns the value at that index
+
+// let string = 'JavaScript';
+// let lastLetter = string[9];
+// console.log(string.charAt(0));
+// console.log(lastLetter);
+
+// let name1 = 'Jeanne';
+// console.log(name1[4]);
+// let name2 = 'Xai';
+// console.log(name2.charAt(0));
+
+// let name3 ='XaijinnGG1';
+// let lastIndex = string.length - 1;
+// console.log(name3.charAt(lastIndex));
+
+//using charCodeAt(); takes index nad it returns char code (ASCII number) of the value at that index;
+
+// let name1 ='Jeanne';
+// console.log(name1.charCodeAt(2));
+// let lastindex = name1.length - 1;
+// console.log(name1.charAt(lastindex));
+
+//using concat(); it takes many substrings and create concatenation
+// let name1 ='Jeanne';
+// console.log(name1.concat("<3Xai"))
+// console.log(name1.concat(" Xai", " Doe", " Dong"))
+
+//using endsWith: it takes a substring as an argument and it checks if the strings starts with that specified substring
+//it returns boleens (true or false)
+//string.endsWith(substring);
+
+// let string = 'Jeanne is a bad guy';
+// console.log(string.endsWith('jeanne'));
+// console.log(string.endsWith('Guy'));
+// console.log(string.endsWith('guy'));
+//case sensitive
+
