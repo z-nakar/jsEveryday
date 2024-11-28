0b70bac3f2163783f3398ad5d9663130b79e054e
@CodeWithZaii <xairelnakar01@gmail.com>
2024-11-28 12:51:37 +0800

    w/outupt

--- script.js
+++ script.js
@@ -2,37 +2,37 @@
 
 // let string = 'JavaScript';
 // let lastLetter = string[9];
-// console.log(string.charAt(0));
-// console.log(lastLetter);
+// console.log(string.charAt(0)); //output: J
+// console.log(lastLetter); //ouput: t
 
 // let name1 = 'Jeanne';
-// console.log(name1[4]);
+// console.log(name1[4]); //output: n
 // let name2 = 'Xai';
-// console.log(name2.charAt(0));
+// console.log(name2.charAt(0)); //output: X
 
 // let name3 ='XaijinnGG1';
 // let lastIndex = string.length - 1;
-// console.log(name3.charAt(lastIndex));
+// console.log(name3.charAt(lastIndex)); //output: 1
 
 //using charCodeAt(); takes index nad it returns char code (ASCII number) of the value at that index;
 
 // let name1 ='Jeanne';
-// console.log(name1.charCodeAt(2));
+// console.log(name1.charCodeAt(2)); //output ASCII
 // let lastindex = name1.length - 1;
-// console.log(name1.charAt(lastindex));
+// console.log(name1.charAt(lastindex)); //output ASCII
 
 //using concat(); it takes many substrings and create concatenation
 // let name1 ='Jeanne';
-// console.log(name1.concat("<3Xai"))
-// console.log(name1.concat(" Xai", " Doe", " Dong"))
+// console.log(name1.concat("<3Xai")) //output: Jeane<3Xai
+// console.log(name1.concat(" Xai", " Doe", " Dong")) //output: Jeane Xai Doe Dong
 
 //using endsWith: it takes a substring as an argument and it checks if the strings starts with that specified substring
 //it returns boleens (true or false)
 //string.endsWith(substring);
 
 // let string = 'Jeanne is a bad guy';
-// console.log(string.endsWith('jeanne'));
-// console.log(string.endsWith('Guy'));
-// console.log(string.endsWith('guy'));
-//case sensitive
+// console.log(string.endsWith('jeanne')); //output: false
+// console.log(string.endsWith('Guy')); //output: false
+// console.log(string.endsWith('guy')); //output: true
+//case sensitive ----
 
